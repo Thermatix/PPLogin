af5b10420f5ec4cf1bf2dfeba406772d7fd1bca8
Thermatix <mbeckerwork@gmail.com>
2014-07-24 23:03:29 +0100

    Deletes rdoc

--- lib/pp_login/main/modules/setup.rb
+++ lib/pp_login/main/modules/setup.rb
@@ -1,9 +1,10 @@
 module PpLogin
 	class << self #stores config variables
-		attr_accessor :id, :secret, :website_url, :mode, :loginURL, :scope
+		attr_accessor :id, :secret, :website_url, :redirect_url, :mode, :loginURL, :scope
 	end
 
 	def self.setup(&block) #setup function for initializer file
 		yield self
+		self.redirect_url = self.website_url if !self.redirect_url
 	end
 end

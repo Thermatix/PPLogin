0f705d6699d32ef3c4846ed6f0f8a55b9c3ecc23
Thermatix <mbeckerwork@gmail.com>
2014-07-25 01:04:59 +0100

    fixes helper syntax error

--- lib/pp_login/version.rb
+++ lib/pp_login/version.rb
@@ -1,5 +1,5 @@
 module PpLogin
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   Desc ="This gem will Add an engine to handle loging in with paypal,
   			 getting user info and logging out again.
 			  "
